97a15a6b38199500e5b989df91b28d07cc95bece
Jorge Mateus <jorgemateus1995@gmail.com>
2024-06-07 17:02:36 -0500

    Se completa la mayor parte de la seccion de proyectos

--- js/script.js
+++ js/script.js
@@ -6,23 +6,51 @@ const projectsData = [
   {
     index: "p1",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nonreprehenderit consectetur quibusdam animi soluta! Eos",
-    src: "./img/Images/Portfolio_Jessica.webp",
-    technologies: ["Node.js", "HTML", "CSS", "Javascript"],
+      "Aplicación fullstack, capaz de realizar todas las operaciones CRUD en una base de datos de Microsoft SQL. Cuenta con sistema de autenticación de usuarios basado en JWT (Json Web Tokens) y encripatación de contraseñas con SHA256. El frontend de Angular cuenta con proteccion de rutas usando sesiones de JWT. El backend es una API hecha con estructura de capas.",
+    src: "./img/Images/CRUD_app.webp",
+    technologies: [
+      "Angular",
+      "Typescript",
+      "CSS",
+      "C#",
+      ".NET Framework",
+      "JWT Authentication",
+      "Microsoft SQL Server",
+    ],
   },
   {
     index: "p2",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nonreprehenderit consectetur quibusdam animi soluta! Eos",
-    src: "./img/Images/Chordmaster.webp",
-    technologies: ["HTML", "CSS", "Javascript"],
+      "Pagina web fullstack, el frontend fue realizado con plantillas PUG. El backend fue diseñado con el modelo MVC utilizando Node.js y Express JS, cuenta con sistema de autenticacion basado en JWT. Para la base de datos se usó MongoDB con funcionalidades avanzadas como queries geograficas.",
+    src: "./img/Images/Growth_Crafter.webp",
+    technologies: [
+      "Node.js",
+      "Express JS",
+      "JWT Authentication",
+      "Pug Templates",
+      "MongoDB",
+    ],
   },
   {
     index: "p3",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nonreprehenderit consectetur quibusdam animi soluta! Eos",
+      "Landing page sencilla realizada a pedido de Growth Crafter para promocionar su curso de analitica de datos en Salesforce, es totalmente responsiva y cuenta con el uso de Google Apps Script para almacenamiento basico de respuestas de un formulario.",
     src: "./img/Images/Growth_Crafter.webp",
     technologies: ["Google Apps Script", "HTML", "CSS", "Javascript"],
+  },
+  {
+    index: "p4",
+    description:
+      "Proyecto personal sencillo para trabajar manipulación basica del DOM, es una aplicación que permite acceder a un gran repertorio de acordes de guitarra, es totalmente responsiva.",
+    src: "./img/Images/Chordmaster.webp",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    index: "p5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nonreprehenderit consectetur quibusdam animi soluta! Eos",
+    src: "./img/Images/Portfolio_Jessica.webp",
+    technologies: ["Node.js", "HTML", "CSS", "Javascript"],
   },
 ];
 
